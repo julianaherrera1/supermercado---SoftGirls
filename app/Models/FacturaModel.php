8898fceb4f00b3411efcdd211696eae0a017c9fd
Juliana Herrera <julianaherrera522@gmail.com>
2025-10-22 10:00:32 -0500

    Fix model: FacturaModel

--- app/Models/FacturaModel.php
+++ app/Models/FacturaModel.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FacturaModel extends Model
 {
-    protected $table = factura;
+    use HasFactory;
+
+    protected $table = 'factura';
+    public $timestamps = true;
+
+    protected $fillable = [
+        // 'cliente_id', 'fecha', 'total', ...
+    ];
+
+    // Relación ejemplo
+    public function detalles()
+    {
+        return $this->hasMany(DetalleModel::class, 'factura_id', 'id');
+    }
 }
